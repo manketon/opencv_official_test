1285d8124370e5edd0c1752b386ba61666a92e8b
manketon <850268547@qq.com>
2024-01-18 19:31:54 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 {
 	int ret = -1;
 	Json::Value jv1, jv2;
-	std::cout << (jv1 == jv2) << std::endl;
+	std::cout << std::boolalpha << (jv1 == jv2) << std::endl;
 	//ret = test_decompose_homography(argc, argv);
 	//ret = test_Sobel(argc, argv);
 	ret = test_load_img_from_bytes();
