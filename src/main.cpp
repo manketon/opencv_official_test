24166e2c3ee4307833f699a32607c68c248577ff
manketon <850268547@qq.com>
2024-01-29 15:24:22 +0800

    1、完善main函数

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,49 @@
 int main(int argc, char* argv[])
 {
 	int ret = -1;
-	Json::Value jv1, jv2;
-	std::cout << std::boolalpha << (jv1 == jv2) << std::endl;
+	//Json::Value jv1, jv2;
+	//std::cout << std::boolalpha << (jv1 == jv2) << std::endl;
 	//ret = test_decompose_homography(argc, argv);
 	//ret = test_Sobel(argc, argv);
 	//ret = test_load_img_from_bytes();
-	std::string str_err_reason;
-	ret = test_demHist(str_err_reason);
+	
+	while (true)
+	{
+		std::cout << "0、退出程序" << std::endl;
+		std::cout << "1、演示灰度直方图calcHist" << std::endl;
+		std::cout << "请输入您的选择:";
+		int nChoise = -1;
+		std::cin >> nChoise;
+		int ret = -1;
+		std::string str_err_reason;
+		switch (nChoise)
+		{
+		case 0:
+			std::cout << "成功退出程序" << std::endl;
+			return 0;
+		case 1:
+			ret = test_demHist(str_err_reason);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		default:
+			std::cout << "非法输入" << std::endl;
+			continue;
+		}
+		if (ret)
+		{
+			std::cout << __FUNCTION__ << " | Has error ret:" << ret << ", reason:" << str_err_reason << std::endl;
+		}
+	}
 	return 0;
 }
