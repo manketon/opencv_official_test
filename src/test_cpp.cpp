e99720baf9b816d84799a5272fff8a8095a5af23
manketon <850268547@qq.com>
2024-10-10 15:59:40 +0800

    1、CustomAllocator：不再继承与std::allocator，而且参考其完全重写一个。

--- src/test_cpp.cpp
+++ src/test_cpp.cpp
@@ -1,28 +1,58 @@
 #include "test_cpp.h"
 #include <iostream>
 #include <vector>
-
-// 自定义分配器
-template<class _Ty>
-class CustomAllocator : public std::allocator<_Ty> {
+#define USING_CUSTOM_ALLOCATOR
+// 自定义分配器（在类型std::allocator基础上修改所得）
+template <class _Ty>
+class CustomAllocator {
 public:
-	template<class U>
-	struct rebind {
-		typedef CustomAllocator<U> other;
+	static_assert(!std::is_const_v<_Ty>, "The C++ Standard forbids containers of const elements "
+		"because allocator<const T> is ill-formed.");
+
+	using _From_primary = CustomAllocator;
+
+	using value_type = _Ty;
+
+#if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS typedef _Ty* pointer;
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS typedef const _Ty* const_pointer;
+
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS typedef _Ty& reference;
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS typedef const _Ty& const_reference;
+#endif // _HAS_DEPRECATED_ALLOCATOR_MEMBERS
+
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+
+	using propagate_on_container_move_assignment = std::true_type;
+
+#if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
+	using is_always_equal _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS = std::true_type;
+
+	template <class _Other>
+	struct _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS rebind {
+		using other = CustomAllocator<_Other>;
 	};
 
-	CustomAllocator() throw() {}
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD _Ty* address(_Ty& _Val) const noexcept {
+		return _STD addressof(_Val);
+	}
 
-	CustomAllocator(const CustomAllocator& other) throw()
-		: std::allocator<_Ty>(other) {}
+	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD const _Ty* address(const _Ty& _Val) const noexcept {
+		return _STD addressof(_Val);
+	}
+#endif // _HAS_DEPRECATED_ALLOCATOR_MEMBERS
 
-	template<class U>
-	CustomAllocator(const CustomAllocator<U>& other) throw()
-		: std::allocator<_Ty>(other) {}
+	constexpr CustomAllocator() noexcept {}
 
-	// 可以在这里添加自定义内存分配逻辑
-
-	_NODISCARD _CONSTEXPR20_DYNALLOC __declspec(allocator) _Ty* allocate(_CRT_GUARDOVERFLOW const size_t _Count) 
+	constexpr CustomAllocator(const CustomAllocator&) noexcept = default;
+	template <class _Other>
+	constexpr CustomAllocator(const CustomAllocator<_Other>&) noexcept {}
+	_CONSTEXPR20_DYNALLOC ~CustomAllocator() = default;
+	_CONSTEXPR20_DYNALLOC CustomAllocator& operator=(const CustomAllocator&) = default;
+	//__declspec(allocator)为Microsoft 专用，声明说明符可应用于自定义内存分配函数，以通过 Windows 事件跟踪 (ETW) 使分配可见
+#ifdef USING_CUSTOM_ALLOCATOR
+	_NODISCARD _CONSTEXPR20_DYNALLOC __declspec(allocator) _Ty* allocate(_CRT_GUARDOVERFLOW const size_t _Count)
 	{
 		std::cout << __FUNCTION__ << " | line:" << __LINE__ << ", Count:" << _Count << std::endl;
 		void* p = ::malloc(std::_Get_size_of_n<sizeof(_Ty)>(_Count));
@@ -37,10 +67,19 @@ public:
 			::free(_Ptr);
 		}
 	}
+#else //与std::allocator中逻辑等价
+	_CONSTEXPR20_DYNALLOC void deallocate(_Ty* const _Ptr, const size_t _Count) {
+		// no overflow check on the following multiply; we assume _Allocate did that check
+		std::_Deallocate<std::_New_alignof<_Ty>>(_Ptr, sizeof(_Ty) * _Count);
+	}
+
+	_NODISCARD _CONSTEXPR20_DYNALLOC __declspec(allocator)_Ty* allocate(_CRT_GUARDOVERFLOW const size_t _Count) {
+		return static_cast<_Ty*>(std::_Allocate<std::_New_alignof<_Ty>>(std::_Get_size_of_n<sizeof(_Ty)>(_Count)));
+	}
+#endif
 #if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
 	_CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD __declspec(allocator) _Ty* allocate(
-		_CRT_GUARDOVERFLOW const size_t _Count, const void*) 
-	{
+		_CRT_GUARDOVERFLOW const size_t _Count, const void*) {
 		std::cout << __FUNCTION__ << " | line:" << __LINE__ << std::endl;
 		return allocate(_Count);
 	}
@@ -69,9 +108,9 @@ int test_CustomAllocator(std::string& str_err_reason)
 	// 使用自定义分配器初始化vector
 	std::vector<int, CustomAllocator<int>> vec(5, -1);
 	// 示例：向vector添加元素
-// 	vec.push_back(1);
-// 	vec.push_back(2);
-// 	vec.push_back(3);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
 
 	// 输出vector中的元素
 	for (int i : vec) {
